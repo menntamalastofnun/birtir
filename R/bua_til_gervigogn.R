3a8d4406d60c47918529a53bbc0b047c308496b6
auv2 <audun@mms.is>
2025-01-09 12:52:50 +0000

    Initial commit

---
+++ R/bua_til_gervigogn.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+data <- tibble(
+  kennitala = "310200-3257",
+  nafn_nemanda = "Grettir Ásmundsson",
+  prof_numer = "les07",
+  dagsetnings_profs = "2025-03-17",
+  profhluti = c("Heildartala", "Orðskilningur",
+                "Djúpur skilningur", "Ályktun",
+                "Bókstaflegur skilningur"),
+  einkunn = c(8, 7, 7, 9, 8)
+)
