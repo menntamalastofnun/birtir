0631f865a3d6587d8ea22ef524e0096fed428c74
auv2 <audun@mms.is>
2025-01-09 12:56:20 +0000

    Initial commit

--- R/bua_til_gervigogn.R
+++ R/bua_til_gervigogn.R
@@ -5,8 +5,10 @@ data <- tibble(
   nafn_nemanda = "Grettir Ásmundsson",
   prof_numer = "les07",
   dagsetnings_profs = "2025-03-17",
-  profhluti = c("Heildartala", "Orðskilningur",
-                "Djúpur skilningur", "Ályktun",
-                "Bókstaflegur skilningur"),
+  profhluti = c(
+    "Heildartala", "Orðskilningur",
+    "Djúpur skilningur", "Ályktun",
+    "Bókstaflegur skilningur"
+  ),
   einkunn = c(8, 7, 7, 9, 8)
 )
