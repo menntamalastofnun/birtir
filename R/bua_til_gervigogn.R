9d0e55a351695b2ea9d7b2495e3a4d4f659b2dae
MarelleM <122885747+MarelleM@users.noreply.github.com>
2025-05-07 12:56:07 +0000

    Breytti data dataframe í bua_til_gervigogn
    Breytti data í df þar sem R var að kalla á fall sem heitir data hjá mér.
    Fækkaði gildum í profhluti úr 5 í 3 og sömuleiðis einkunn.

--- R/bua_til_gervigogn.R
+++ R/bua_til_gervigogn.R
@@ -1,19 +1,32 @@
 # Dæmi um nemendur --------------------------------------------------------
 
 
-data <- tibble::tibble(
+# df <- tibble::tibble(
+#   kennitala = "310200-3257",
+#   nafn_nemanda = "Grettir Ásmundsson",
+#   prof_numer = "les07",
+#   dagsetnings_profs = "2025-03-17",
+#   profhluti = c(
+#     "Heildartala", "Orðskilningur",
+#     "Djúpur skilningur", "Ályktun",
+#     "Bókstaflegur skilningur"
+#   ),
+#   einkunn = c(8, 7, 7, 9, 8)
+# )
+
+
+df <- tibble::tibble(
   kennitala = "310200-3257",
   nafn_nemanda = "Grettir Ásmundsson",
   prof_numer = "les07",
   dagsetnings_profs = "2025-03-17",
   profhluti = c(
-    "Heildartala", "Orðskilningur",
-    "Djúpur skilningur", "Ályktun",
-    "Bókstaflegur skilningur"
+    "Heildartala",
+    "Leita og finna upplýsingar",
+    "Túlkun og ígrundun upplýsinga"
   ),
-  einkunn = c(8, 7, 7, 9, 8)
+  einkunn = c(8, 7, 9)
 )
-
 
 # Dæmi um kvarða ----------------------------------------------------------
 
