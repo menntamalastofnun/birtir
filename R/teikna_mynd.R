c3b433967706b44f7640b489775624c1f2157623
MarelleM <122885747+MarelleM@users.noreply.github.com>
2025-05-07 13:15:02 +0000

    Breytingar á theme í lysa_stodu
    Smávægilegar breytingar á útliti lysa_stodu

--- R/teikna_mynd.R
+++ R/teikna_mynd.R
@@ -74,9 +74,9 @@ lysa_stodu <- function(data, kvardi) {
       name = "Mælitala"
     ) +
     theme(
-      line = element_line(linetype = 1, colour = "black"),
+      line = element_line(linetype = 1, colour = "#292A4B"),
       axis.text = element_text(face = "bold", size = rel(1)),
-      text = element_text(colour = "black"),
+      text = element_text(colour = "#292A4B"),
       rect = element_rect(
         fill = NULL,
         linetype = 0,
@@ -87,7 +87,7 @@ lysa_stodu <- function(data, kvardi) {
       # legend.direction = "horizontal",
       # legend.box = "vertical",
       panel.grid = element_line(colour = NULL, linetype = 3),
-      panel.grid.major = element_line(colour = "black"),
+      panel.grid.major = element_line(colour = "#292A4B"),
       panel.grid.major.x = element_blank(),
       panel.grid.minor = element_blank(),
       panel.background = element_blank(),
@@ -97,12 +97,12 @@ lysa_stodu <- function(data, kvardi) {
       strip.background = element_rect(),
       axis.ticks.y = element_blank(),
       axis.title.x = element_blank(),
-      axis.title.y = element_blank(),
+      #axis.title.y = element_blank(),
       axis.text.x = element_blank(),
       axis.ticks.x = element_blank(),
       axis.line.x = element_blank(),
       axis.line.y = element_line(
-        linewidth = 1.5#,
+        linewidth = 1#,
         #arrow = grid::arrow(length = unit(0.3, "cm"), ends = "both")
       ),
       plot.margin = unit(c(1, 1, 2, 1), "cm")
