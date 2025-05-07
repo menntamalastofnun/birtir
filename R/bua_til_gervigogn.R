64baba45fc6f21dae0b99cf87fcacc76deb52547
MarelleM <122885747+MarelleM@users.noreply.github.com>
2025-05-07 13:01:38 +0000

    Breytti umsögnum og kvarda í þrennt
    Umsagnir eru nú þrjár í staðin fyrir 4.
    Einkunn og lysing inniheldur þrjú gildi í staðin fyrir fjögur.

--- R/bua_til_gervigogn.R
+++ R/bua_til_gervigogn.R
@@ -33,17 +33,17 @@ df <- tibble::tibble(
 umsogn <- c(
   "Nemandi skilur illa textann. \n",
   "Nemandi skilur textann að hluta, og getur fundið einfaldar upplýsingar  í texta. \n\n",
-  "Nemandinn skilur innihald textans, getur auðveldlega fundið upplýsingar í textanum og getur dregið ályktanir. \n\n",
-  "Nemandi sýnir góðan skilning á þeim texta sem hann les, getur lesið á milli línanna og dregið flóknar ályktanir. \n"
+  "Nemandinn skilur innihald textans, getur auðveldlega fundið upplýsingar í textanum og getur dregið ályktanir. # \n\n",
+  #"Nemandi sýnir góðan skilning á þeim texta sem hann les, getur lesið á milli línanna og dregið flóknar ályktanir. \n"
 
 )
 
 kvardi <- list(
   kvardi_bil = c(0,20),
   kvardi_lysing = tibble::tibble(
-    einkunn = c(3, 6, 10, 15),
+    einkunn = c(7, 12, 20),
     lysing = c("Þarfnast mikillar þjálfunar",
-               "Þarfnast þjálfunar",
+               #"Þarfnast þjálfunar",
                "Á góðri leið",
                "Framúrskarandi"),
     umsogn = factor(umsogn, levels = umsogn)
