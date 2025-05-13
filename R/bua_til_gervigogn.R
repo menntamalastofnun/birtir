f6e90afa165d7ba5ebefc76ca6e80e280a7023d2
MarelleM <122885747+MarelleM@users.noreply.github.com>
2025-05-13 08:49:19 +0000

    Update utbua_gervigogn
    Færði df_bekkur og df_atridi úr scriptunni.

--- R/bua_til_gervigogn.R
+++ R/bua_til_gervigogn.R
@@ -37,55 +37,6 @@ kvardi <- list(
 )
 
 
-# Dæmi um bekkjargögn ----------------------------------------------------------
-df_bekkur <- tibble::tibble(
-  #kennitala = "310200-3257",
-  nafn_nemenda = c("Grettir Ásmundsson",
-                   "Helena Birgisdóttir",
-                   "María Birna Karlsdóttir",
-                   "Patrekur Sóleyjarson",
-                   "Pétur Vilhjálmsson",
-                   "Agnes Kristjánsdóttir",
-                   "Maríanna Hekla Tómasdóttir"),
-  prof = "les07",
-  dagsetnings_profs = "2025-03-17",
-  maelitala = c(2, 7, 5, 8, 9, 3, 11),
-  sfmt = c(0.8, 1.2, 1, 0.9, 1.1, 0.8, 1.3)
-  # profhluti = c(
-  #   "Heildartala",
-  #   "Leita og finna upplýsingar",
-  #   "Túlkun og ígrundun upplýsinga"
-  # )
-  # #einkunn = c(8, 7, 9)
-)
-
-
-
-# Dæmi um gögn á atriðaleveli ----------------------------------------------------------
-library(tibble)
-library(dplyr)
-
-set.seed(42)  # for reproducibility
-
-# Define sample values
-nafn <- c("Grettir Ásmundsson", "Helena Birgisdóttir", "María Birna Karlsdóttir", "Patrekur Sóleyjarson", "Pétur Vilhjálmsson")
-kennitala <- c("310200-3257", "250201-4489", "180300-8765", "041199-1123", "120200-5567")
-item_ids <- paste0("Atriði", 1:10)
-difficulty_levels <- c("Mjög létt", "Létt", "Þungt", "Mjög þungt")
-
-# Create the dummy dataframe
-df_atridi <- expand.grid(
-  nafn = nafn,
-  item_id = item_ids
-) %>%
-  mutate(
-    kennitala = rep(kennitala, each = length(item_ids)),
-    difficulty = sample(difficulty_levels, size = n(), replace = TRUE),
-    student_response = sample(c(TRUE, FALSE), size = n(), replace = TRUE, prob = c(0.7, 0.3))
-  )
-
-df_atridi <- as_tibble(df_atridi)
-
 
 
 
