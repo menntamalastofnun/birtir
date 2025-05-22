53a7dab6a12d54ab8adbffa7d85800615d2dbcc6
MarelleM <122885747+MarelleM@users.noreply.github.com>
2025-05-22 15:26:13 +0000

    Uppfært lysa_stodu_bekkjar()
    Uppfærði lysa_stodu_bekkjar fallið þannig að nú birtist heildarmælitala og undirþættir á sömu mynd.

--- R/teikna_mynd_bekkjar.R
+++ R/teikna_mynd_bekkjar.R
@@ -1,9 +1,3 @@
-library(purrr)
-library(rlang)
-library(ggplot2)
-library(readxl)
-
-
 #' Mynd af stadsetningu nemanda a kvarda asamt lysingu
 #'
 #' @param data einkunnir nemanda
@@ -11,46 +5,62 @@ library(readxl)
 #' @importFrom dplyr filter
 #' @importFrom stringr str_wrap
 #' @import ggplot2
-#' @returns skilar mynd af stadsettningu nemanda
+#' @returns skilar mynd af stadsetningu bekkjar midad vid landsmedaltal a ollu profinu og undirthattum
 #' @export
 #'
-#' @examples lysa_stodu(fa_heildartolu(5), fa_kvarda())
+#' @examples lysa_stodu_bekkjur(df, kvardi)
 #'
 lysa_stodu_bekkjar <- function(data, kvardi) {
-  heildartala <- data |>
-    dplyr::summarise(
-      medalmaelitala = mean(maelitala, na.rm = TRUE), # reikna meðaltal bekkjar
-      sf = sqrt(sum(sfmt^2)) / nrow(data), # reikna staðalfrávik bekkjar (athuga hvaða upplýsingar við erum að birta hér og hvað við erum að taka inn!!!!!)
+  # Compute stats per profhluti
+  heildartolur <- data |>
+    group_by(profhluti) |>
+    summarise(
+      medalmaelitala = mean(maelitala, na.rm = TRUE),
+      sf = sqrt(sum(sfmt^2, na.rm = TRUE)) / n(),
       .groups = "drop"
     )
 
-
-
-  kvardi_bil <- kvardi$kvardi_bil # kvardi bil nær frá 0 til 20
+  kvardi_bil <- kvardi$kvardi_bil
   kvardi_lysing <- kvardi$kvardi_lysing
-  kvardi_texta_bil <- c(2, 5, 9.5, 15.5)
-
-  if (nrow(heildartala) != 1) {
-    stop(
-      "Gagnasettid inniheldur ekki profhlutan heildartolu.
-      Athugid hvort nafnid se ekki rett skrifad Heildartala."
-    )
-  }
+  # kvardi_texta_bil <- c(2, 5, 9.5, 15.5)
 
 
-  fjardlaegd_punkts_fra_texta <- .5
-
-
-  heildartala |>
-    ggplot(aes(.1, medalmaelitala)) +
+  ggplot() +
+    # Background scale (across y-axis only, not x)
     litud_maelistika(
-      y_range = c(kvardi_bil[1]+1, kvardi_bil[2]-1),
+      y_range = c(kvardi_bil[1], kvardi_bil[2]),
       cutoffs = kvardi_lysing$einkunn,
       alpha = 1,
-      litur = "#D8C1FF" # Breytti litnum úr #C7FBD2
+      litur = "#D8C1FF"
     ) +
+    # Student-level points (jittered horizontally)
+    geom_jitter(
+      data = data,
+      aes(x = profhluti, y = maelitala),
+      width = 0.1,
+      height = 0,
+      shape = 21,
+      color = "#292A4B",
+      fill = "#292A4B",
+      alpha = 0.3,
+      size = 3
+    ) +
+    # Mean point per profhluti
+    geom_point(
+      data = heildartolur,
+      aes(x = profhluti, y = medalmaelitala),
+      size = 5,
+      shape = 21,
+      stroke = 1,
+      color = "#292A4B",
+      fill = "#292A4B"
+    ) +
+
+    # Error bars per profhluti
     geom_errorbar(
+      data = heildartolur,
       aes(
+        x = profhluti,
         ymin = medalmaelitala - sf,
         ymax = medalmaelitala + sf
       ),
@@ -58,81 +68,31 @@ lysa_stodu_bekkjar <- function(data, kvardi) {
       color = "#292A4B",
       linewidth = 0.6
     ) +
-    # Bæta við upplýsingum um stöðu einstaka nemenda - hálf gegnsæir punktar með mælitölu hvers nemanda
-    geom_jitter(
-      data = data,
-      aes(x = 0.05, y = maelitala),
-      width = 0.01,
-      height = 0,
-      shape = 21,
+    geom_hline(
+      yintercept = 10,
+      linetype = "dotted",
       color = "#292A4B",
-      fill = "#292A4B",
-      alpha = 0.3,
-      size = 3,
-      stroke = 1
-    ) +
-    geom_point(
-      size = 3, # var 10
-      shape = 21,
-      stroke = 1,
-      color = "#292A4B", # Breytt úr "black"
-      fill = "#292A4B", # Breytt úr "black"
-      show.legend = F
-    ) +
-    # geom_label(
-    #   data = kvardi_lysing,
-    #   aes(
-    #     x = fjardlaegd_punkts_fra_texta,
-    #     y = einkunn,
-    #     label = stringr::str_wrap(umsogn , width = 40)
-    #     #label = padded_umsogn # Bætti við padded_umsogn í reprt_tmpl þegar ég var að búa til kvardann
-    #   ),
-    #   #fill = "#c7fbd2",
-    #   label.size = NA,
-    #   fill = "white", # bætti við
-    #   #color = "black",
-    #   #show.legend = F,
-    #   hjust = 0,
-    #   vjust = 0
-    # )
-    geom_text(
-      data = kvardi_lysing,
-      aes(
-        x = fjardlaegd_punkts_fra_texta,
-        y = kvardi_texta_bil,  # use the adjusted y-coordinate
-        label = stringr::str_wrap(umsogn, width = 40)
-      ),
-      hjust = 0,
-      vjust = 0.5,  # center vertically
-      color = "#292A4B",
-      size = 3.5
+      linewidth = 1
     ) +
 
-    # Teiknar lýsingartextann fyrir hvert bilc
-    scale_x_continuous(limits = c(0, 1.5)) + # breytti limits úr c(0, 1)
+
     scale_y_continuous(
       limits = kvardi_bil,
-      breaks = seq(from  = kvardi_bil[1]+1, to = kvardi_bil[2]-1, by = 1),
+      breaks = seq(kvardi_bil[1] + 1, kvardi_bil[2] - 1, by = 1),
       name = "Mælitala"
     ) +
-    #scale_y_continuous(
-    # breaks = kvardi_lysing$einkunn,
-    #labels = kvardi_lysing$lysing,
-    #limits = kvardi_bil
-    #) +
+
+    scale_x_discrete(labels = function(x) str_wrap(x, width = 15)) + # færir textann á x-ás í nýja línu ef hann er of langur
     theme(
-      line = element_line(linetype = 1, colour = "#292A4B"), # Breytti litnum úr "black"
+      line = element_line(linetype = 1, colour = "#292A4B"),
       axis.text = element_text(face = "bold", size = rel(1)),
-      text = element_text(colour = "#292A4B"), # Breytti litnum úr "black"
+      text = element_text(colour = "black"),
       rect = element_rect(
         fill = NULL,
         linetype = 0,
         colour = NA
       ),
       legend.background = element_rect(fill = NULL),
-      # legend.position = "top",
-      # legend.direction = "horizontal",
-      # legend.box = "vertical",
       panel.grid = element_line(color = NULL, linetype = 3),
       panel.grid.major = element_line(colour = "#292A4B"),
       panel.grid.major.x = element_blank(),
@@ -145,20 +105,16 @@ lysa_stodu_bekkjar <- function(data, kvardi) {
       strip.background = element_rect(),
       axis.ticks.y = element_blank(),
       axis.title.x = element_blank(),
-      #axis.title.y = element_blank(),
-      axis.text.x = element_blank(),
+      # axis.title.y = element_blank(),
+      #axis.text.x = element_blank(),
       axis.ticks.x = element_blank(),
       axis.line.x = element_blank(),
       axis.line.y = element_line(
         linewidth = 1#, # Breytti úr 1.5
-        #arrow = grid::arrow(length = unit(0.3, "cm"), ends = "both")
-      ),
-      plot.margin = unit(c(1, 1, 2, 1), "cm")
-    ) +
-    coord_equal(ratio = 1 / 10) # Breytti úr 1/20 til að hækka grafið
+      )
+    )
 }
 
-#lysa_stodu_bekkjar(df_bekkur, kvardi)
 
 #############################################################################
 
